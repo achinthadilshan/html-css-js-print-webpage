6b039fc7c6a143a2685c00b8889bfeaf238dab0d
Achintha Dilshan <n.achintha.dilshan@gmail.com>
2023-04-18 21:19:35 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
+const printDiv = () => {
+   var divContents = document.getElementById('printable-content').innerHTML
+   var a = window.open('', '')
+   a.document.write('<html>')
+   a.document.write('<body >')
+   a.document.write(divContents)
+   a.document.write('</body></html>')
+   a.print()
+   a.close()
+}
+
 const printBtn = document.getElementById('printBtn')
 
 printBtn.addEventListener('click', () => {
-   window.print()
+   printDiv()
 })
